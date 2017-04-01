df39d04496e7bfd5514d2f5e1b4f9bf72f61cd73
Jonathan Harris <thequattro@Jonathan-Harriss-MacBook-Pro.local>
2017-03-31 22:45:09 -0500

    new work

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "UhaZLYP5nkdB3e0TAAqMFInLE",
+  consumer_secret: 	"0Sm5HmS79E2O0KFKV5wMIbHhFYW1mBgCHNbTSRQADJZMMRBMhw",
+  access_token_key: '800029751016652800-iRT2Q2rZQLXLGsQ4cVvcOEK2944qgNx',
+  access_token_secret: "uzzGAUJ9LjUcokQJSUOXKc6jsNj5HVZdWSoQ0U6N2Btj3",
+}
---
+++ liri.js
@@ -0,0 +1,7 @@
+// npm installs
+var request = require("request");
+var twitter = require("twitter");
+var spotify = require("spotify");
+
+// ask the user what they want to do which means adding in the inquirer so you can use prompt. build out functions for each question.
+//  lets do omdb first. you are going to need
